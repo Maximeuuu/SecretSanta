9ae7b34080ddcf4925a3f1f669987cce41adf83c
Maximeuuu <maxime.lemoine374@gmail.com>
2024-01-14 10:54:26 +0100

    copie du src

---
+++ src/SecretSanta.java
@@ -0,0 +1,33 @@
+import iut.algo.*;
+
+/* java SecretSanta
+* Auteur : Maxime L
+* Version : 1 du 27/11/22
+*/
+
+public class SecretSanta
+{
+
+	public static void main(String[] argv)
+	{
+		/*-------------------------------*/
+		/* DONNEES                       */
+		/*-------------------------------*/
+
+		/*constantes*/
+		final int PRIX = 10;
+
+		/*variables*/
+		String[] tabPers;
+
+		/*-------------------------------*/
+		/* INSTRUCTIONS                  */
+		/*-------------------------------*/
+		
+		tabPers = new String[]
+		tabPers = {"Maxime", "Alexandre", "Gregoire", "Camille", "Anais", "Marcus", "Leane", "Lars"}
+
+		System.out.println("Sortie");
+	}
+
+}
